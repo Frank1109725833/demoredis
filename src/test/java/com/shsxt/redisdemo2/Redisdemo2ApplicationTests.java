4300297477566e41b202781711c7de0ad381c5b3
ymd <1109725833@qq.com>
2020-10-04 15:05:32 +0800

    初次提交2

--- src/test/java/com/shsxt/redisdemo2/Redisdemo2ApplicationTests.java
+++ src/test/java/com/shsxt/redisdemo2/Redisdemo2ApplicationTests.java
@@ -46,6 +46,5 @@ public class Redisdemo2ApplicationTests {
         list.add("4");
         List<Object> objectList = value.multiGet(list);
         objectList.forEach(System.out::println);
-
     }
 }
