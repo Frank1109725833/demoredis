3227991750cdcccbd493c0dac6352699c0b940d8
ymd <1109725833@qq.com>
2020-10-04 14:59:32 +0800

    初次提交

--- src/test/java/com/shsxt/redisdemo2/Redisdemo2ApplicationTests.java
+++ src/test/java/com/shsxt/redisdemo2/Redisdemo2ApplicationTests.java
@@ -7,6 +7,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 public class Redisdemo2ApplicationTests {
@@ -24,7 +28,24 @@ public class Redisdemo2ApplicationTests {
         user.setAge(24);
         valueOperations.set("User",user);
         System.out.println(valueOperations.get("User"));
+    }
 
+    @Test
+    public void test2 (){
+        ValueOperations<String, Object> value = redisTemplate.opsForValue();
+        Map<String,String> map=new HashMap<>();
+        map.put("1","大娃");
+        map.put("2","二娃");
+        map.put("3","三娃");
+        map.put("4","四娃");
+        value.multiSet(map);
+        List<String> list=new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        List<Object> objectList = value.multiGet(list);
+        objectList.forEach(System.out::println);
     }
 
 }
